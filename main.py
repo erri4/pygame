414f235b8e9f534e0831f77129e139d0d03f937d
rif av <reef.abramson@gmail.com>
2024-12-30 16:26:43 +0200

    .

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-# Imports
 import pygame as game  
 from os.path import join
 import random
 
-# general setup
-game.init() # Initializes all Pygame modules.
-width, height = 1280, 720 # Sets the dimensions of the game window.
-display = game.display.set_mode((width, height), game.RESIZABLE) # Creates the game window with the specified size and makes it resizable.
-game.display.set_caption("reef and liam's game") # Sets the title of the window.
 
-# Game Variables
+game.init() 
+screen = game.display.set_mode()
+width, height = screen.get_size()[0] - 100, screen.get_size()[1] - 100
+display = game.display.set_mode((width, height), game.RESIZABLE)
+game.display.set_caption("reef and liam's game") 
+
+
 speed = 10
 running = True
 clock = game.time.Clock()
 
-# Creating a Player Surface
-player_surf = game.Surface((100,200)) # We create a surface for the player and fill it with an orange color.
+
+player_surf = game.Surface((100,200)) 
 player_surf.fill("orange")
 player_direction = game.math.Vector2()
 player_speed = 300
 
-#importing an image
-player = game.image.load(join("5games", "space shooter", "images", "player.png")).convert_alpha()  # Loads images for the player 
-player_rect= player.get_frect(center = (width / 2,height / 2)) # sets the player position in this case its in the center
+
+player = game.image.load(join("5games", "space shooter", "images", "player.png")).convert_alpha()
+player_rect= player.get_frect(center = (width / 2,height / 2)) 
 object = game.image.load(join("5games", "space shooter", "images", "star.png")).convert_alpha()
 object_positions = [(random.randint(0 , width),random.randint(0,height)) for i in range (20)]
 meteor = game.image.load(join("5games", "space shooter", "images", "meteor.png"))
@@ -31,13 +31,12 @@ laser = game.image.load(join("5games", "space shooter", "images", "laser.png"))
 laser_rect = laser.get_frect(bottomleft = ( 20 ,height - 20))
 
 while running:
-    #event loop
     dt = clock.tick() / 1000
-    for event in game.event.get(): #checks for events if user closes this window we running to be false
+    for event in game.event.get(): 
         if event.type == game.QUIT:
             running = False
         
-    #input    
+
     keys = game.key.get_pressed()
     player_direction.x = int(keys[game.K_RIGHT]) - int(keys[game.K_LEFT])
     player_direction.y = int(keys[game.K_DOWN]) - int(keys[game.K_UP])
@@ -47,17 +46,14 @@ while running:
     a = recent_keys[game.K_SPACE]
     if a:
         print("fire laser")
-    # draw the game
-    display.fill("grey") #We fill the background with grey and draw the stars, meteor, and laser on the display.
+    display.fill("grey")
     for pos in object_positions:
         display.blit(object, pos)
     display.blit(meteor,(meteor_rect))  
     display.blit(laser,(laser_rect))
     
-     
-    # player rendering        
-    display.blit(player,(player_rect)) # Draws the player image at the updated position.
-    game.display.update() # Refreshes the display to show the latest changes.
+           
+    display.blit(player,(player_rect)) 
+    game.display.update()
 
-# Quit Pygame
-game.quit()        
+game.quit()
