4b0aac2f4aebc9b9ce5f329500458d39701acb7a
rif av <reef.abramson@gmail.com>
2024-12-30 16:05:09 +0200

    steal liam's code

--- main.py
+++ main.py
@@ -1,39 +1,63 @@
-import pygame as game
-import os
+# Imports
+import pygame as game  
+from os.path import join
 import random
 
+# general setup
+game.init() # Initializes all Pygame modules.
+width, height = 1280, 720 # Sets the dimensions of the game window.
+display = game.display.set_mode((width, height), game.RESIZABLE) # Creates the game window with the specified size and makes it resizable.
+game.display.set_caption("reef and liam's game") # Sets the title of the window.
 
-game.init()
-
-size = (640, 310)
-
-display = game.display.set_mode(size, game.RESIZABLE)
-
-icon = game.image.load('reef.jpg').convert()
-player = game.image.load(os.path.join('5games', 'space shooter', 'images', 'player.png')).convert_alpha()
-star_path = os.path.join('5games', 'space shooter', 'images', 'star.png')
-
-game.display.set_caption("reef and liam's game")
-game.display.set_icon(icon)
-
+# Game Variables
+speed = 10
 running = True
-surf = game.Surface((100, 100))
-surf.fill((255, 0, 0))
+clock = game.time.Clock()
+
+# Creating a Player Surface
+player_surf = game.Surface((100,200)) # We create a surface for the player and fill it with an orange color.
+player_surf.fill("orange")
+player_direction = game.math.Vector2()
+player_speed = 300
+
+#importing an image
+player = game.image.load(join("5games", "space shooter", "images", "player.png")).convert_alpha()  # Loads images for the player 
+player_rect= player.get_frect(center = (width / 2,height / 2)) # sets the player position in this case its in the center
+object = game.image.load(join("5games", "space shooter", "images", "star.png")).convert_alpha()
+object_positions = [(random.randint(0 , width),random.randint(0,height)) for i in range (20)]
+meteor = game.image.load(join("5games", "space shooter", "images", "meteor.png"))
+meteor_rect = meteor.get_frect(center = (width / 2,height /2))
+laser = game.image.load(join("5games", "space shooter", "images", "laser.png"))
+laser_rect = laser.get_frect(bottomleft = ( 20 ,height - 20))
 
 while running:
-    for event in game.event.get():
+    #event loop
+    dt = clock.tick() / 1000
+    for event in game.event.get(): #checks for events if user closes this window we running to be false
         if event.type == game.QUIT:
             running = False
-            break
+        
+    #input    
+    keys = game.key.get_pressed()
+    player_direction.x = int(keys[game.K_RIGHT]) - int(keys[game.K_LEFT])
+    player_direction.y = int(keys[game.K_DOWN]) - int(keys[game.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction  
+    player_rect.center += player_direction * player_speed * dt 
+    recent_keys = game.key.get_just_pressed()
+    a = recent_keys[game.K_SPACE]
+    if a:
+        print("fire laser")
+    # draw the game
+    display.fill("grey") #We fill the background with grey and draw the stars, meteor, and laser on the display.
+    for pos in object_positions:
+        display.blit(object, pos)
+    display.blit(meteor,(meteor_rect))  
+    display.blit(laser,(laser_rect))
     
-    if running == False:
-        break
+     
+    # player rendering        
+    display.blit(player,(player_rect)) # Draws the player image at the updated position.
+    game.display.update() # Refreshes the display to show the latest changes.
 
-    for i in range(20):
-        globals()[f'star{i}'] = game.image.load(star_path).convert_alpha()
-        display.fill((0, 255, 0))
-        pos = (random.randint(0, 640), random.randint(0, 310))
-        display.blit(globals()[f'star{i}'], pos)
-    game.display.update()
-
+# Quit Pygame
 game.quit()
